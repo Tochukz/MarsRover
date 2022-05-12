1b60a4f1984b304df4cb09a568a361cb8cdcfa66
Chucks Nwachukwu <chucks@bbd.co.za>
2022-05-12 18:15:46 +0200

    Implemented Mars Rover Nagivator

---
+++ MarsRover/Navigator.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MarsRover
+{
+	class Navigator
+	{
+		int[] UpperRightCoord = new int[2];
+		Dictionary<char, int> Directions = new Dictionary<char, int>
+		{
+			{'W', 1 },
+			{'N', 2 },
+			{'E', 3 },
+			{'S', 4 }
+		};
+
+		Rover rover;
+
+		public void SetUpperRight(int x, int y)
+		{
+			UpperRightCoord[0] = x;
+			UpperRightCoord[1] = y;
+		}
+
+		public void SetRover(int x, int y, char d)
+		{
+			rover = new Rover { X = x, Y = y, Direction = Directions[d]};
+		}
+
+		public Rover Navigate(string letters)
+		{
+			if (rover == null)
+			{
+				Console.WriteLine("No rover deployed yet!");
+				return null;
+			}
+			char[] movements = letters.ToCharArray();
+
+			foreach(char c in movements)
+			{
+				if (c == 'M')
+				{
+					Move(rover);
+				}
+				else if(c == 'L' || c == 'R')
+				{
+					Rotate(rover, c);
+				}
+				else
+				{
+					Console.WriteLine($"Invalid command character {c}");
+				}
+
+				
+			}
+			return rover;
+		}
+
+		private void Rotate(Rover rover, char c)
+		{
+			int currentDirection = rover.Direction;
+			if (c == 'R')
+			{
+				int next = (currentDirection + 1 > 4) ? 1 : (currentDirection + 1);
+				rover.Direction = next;
+			}
+			else if (c == 'L')
+			{
+				int next = (currentDirection - 1 <= 0) ? 4 : (currentDirection - 1);
+				rover.Direction = next;
+			}
+		}
+
+		private void Move(Rover rover)
+		{
+			switch(rover.Direction)
+			{
+				case 1:
+					rover.X--;
+					break;
+				case 2:
+					rover.Y++;
+					break;
+				case 3:
+					rover.X++;
+					break;
+				case 4:
+					rover.Y--;
+					break;
+				default:
+					Console.WriteLine($"Unknown direction {rover.Direction}");
+					break;
+			}
+
+			// The rover must not be allowed to navigate beyond the perimeter of the plain 
+			if (rover.X < 0)
+			{
+				rover.X = 0;
+			}
+			else if (rover.X > UpperRightCoord[0])
+			{
+				rover.X = UpperRightCoord[0];
+			}
+			
+			if(rover.Y < 0)
+			{
+				rover.Y = 0;
+			}
+			else if (rover.Y > UpperRightCoord[1])
+			{
+				rover.Y = UpperRightCoord[1];
+			}
+		}
+
+		public void PrintPosition()
+		{
+			char[] keys = Directions.Keys.ToArray();
+			char d = keys[rover.Direction - 1];
+			Console.WriteLine($"{rover.X} {rover.Y} {d}");
+		}
+	}
+
+	class Rover
+	{
+		public int X;
+
+		public int Y;
+
+		public int Direction;
+	}
+}
---
+++ MarsRover/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MarsRover
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Navigator navigator = new Navigator();
+			navigator.SetUpperRight(5, 5);
+			navigator.SetRover(1, 2, 'N');
+			navigator.Navigate("LMLMLMLMM");
+			navigator.PrintPosition();
+
+			navigator.SetRover(3, 3, 'E');
+			navigator.Navigate("MMRMMRMRRM");
+			navigator.PrintPosition();
+			Console.ReadLine();
+		}
+	}
+}
